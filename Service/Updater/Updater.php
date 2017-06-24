ddb35377e09127d577dd5529a4ebab21c2141e21
polonair <fixnim@gmail.com>
2017-06-24 13:57:46 +0400

    Изменения инициированные модификацией шлюза

--- Service/Updater/Updater.php
+++ Service/Updater/Updater.php
@@ -42,20 +42,21 @@ class Updater
 		$worker = null;
 		switch($job->getName())
 		{
-			case "save": $worker = new SaveWorker($job, $this->doctrine); break; 
-			case "bill_out": $worker = new BillWorker($job, $this->doctrine); break; 
-			case "event_process": $worker = new EventProcessor($job, $this->doctrine); break;
-			case "update_spreads": $worker = new UpdateSpreadsWorker($job, $this->doctrine); break; 
-			case "close_tasks": $worker = new CloseTasksWorker($job, $this->doctrine); break; 
-			case "open_tasks": $worker = new OpenTasksWorker($job, $this->doctrine); break; 
-			case "clear_db": $worker = new ClearDbWorker($job, $this->doctrine); break;
-			case "send_list": $worker = new SendListWorker($job, $this->doctrine, $this->sms_sender); break;
-			case "sync_tasks": $worker = new SyncTasksWorker($job, $this->doctrine); break;
-			case "sync_routes": $worker = new SyncRoutesWorker($job, $this->doctrine); break;
-			case "sync_dongles": $worker = new SyncDonglesWorker($job, $this->doctrine); break;
-			case "check_accounts": $worker = new AccountCheckWorker($job, $this->doctrine); break;
-			case "archive_records": $worker = new ArchiveRecordsWorker($job, $this->doctrine); break;
-			case "unhold_transactions": $worker = new UnholdTransactionsWorker($job, $this->doctrine); break;
+			case "save": $worker = new SaveWorker($job, $this->doctrine); break; /* + */
+			case "bill_out": $worker = new BillWorker($job, $this->doctrine); break; /* + */
+			case "event_process": $worker = new EventProcessor($job, $this->doctrine); break; /* + */
+			case "update_spreads": $worker = new UpdateSpreadsWorker($job, $this->doctrine); break; /* + */
+			case "close_tasks": $worker = new CloseTasksWorker($job, $this->doctrine); break; /* + */
+			case "open_tasks": $worker = new OpenTasksWorker($job, $this->doctrine); break; /* + */
+			case "clear_db": $worker = new ClearDbWorker($job, $this->doctrine); break;/* - */
+			case "send_list": $worker = new SendListWorker($job, $this->doctrine, $this->sms_sender); break;/* + */
+			case "sync_tasks": $worker = new SyncTasksWorker($job, $this->doctrine); break;/* +- */
+			//case "sync_masters": $worker = new SyncMastersWorker($job, $this->doctrine); break;/* - */
+			case "sync_routes": $worker = new SyncRoutesWorker($job, $this->doctrine); break;/* - */
+			case "sync_dongles": $worker = new SyncDonglesWorker($job, $this->doctrine); break;/* + */
+			case "check_accounts": $worker = new AccountCheckWorker($job, $this->doctrine); break;/* - */
+			case "archive_records": $worker = new ArchiveRecordsWorker($job, $this->doctrine); break;/* - */
+			case "unhold_transactions": $worker = new UnholdTransactionsWorker($job, $this->doctrine); break;/* + */
 			default: break;
 		}
 		if ($worker !== null) $worker->doJob();
