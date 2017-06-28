a4b672988bc1fbd84005dc78ac7d6b832fc58299
polonair <fixnim@gmail.com>
2017-06-28 20:31:56 +0400

    Небольшие доработки
    Изменено:
      - Service/Updater/Worker/UpdateDonglesWorker.php - реализовано обновление
        донглов;
      - Service/Updater/Worker/UpdateSpreadsWorker.php - исправлена ошибка при
        пересчете спредов.

--- Service/Updater/Worker/UpdateDonglesWorker.php
+++ Service/Updater/Worker/UpdateDonglesWorker.php
@@ -14,7 +14,34 @@ class UpdateDonglesWorker
     }
     public function doJob()
     {
+        $em = $this->doctrine->getManager();
+        $dongle = $em->getRepository("ModelBundle:Dongle")->loadEarlyUpdatedDongle();
+
     	// touch dongle
-    	// check balance
+        $touchUrl = sprintf("https://sm.megafon.ru/sm/client/routing/set?login=%s@multifon.ru&password=%s&routing=1", 
+                            $dongle->getNumber(), $dongle->getPassVoice());
+        $ch = curl_init($touchUrl);
+        curl_setopt($ch, CURLOPT_SSLVERSION,3);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE); 
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);  
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HEADER, false);
+        curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+        $response = curl_exec($ch);
+        curl_close($ch);
+        // check balance
+        $balanceUrl = sprintf("https://sm.megafon.ru/sm/client/balance/?login=%s@multifon.ru&password=%s&routing=1", 
+                            $dongle->getNumber(), $dongle->getPassVoice());
+        $ch = curl_init($balanceUrl);
+        curl_setopt($ch, CURLOPT_SSLVERSION,3);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE); 
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);  
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HEADER, false);
+        curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+        $response = curl_exec($ch);
+        curl_close($ch);
+        $response = new SimpleXMLElement($response);
+        if ($response->result->code == 200) $dongle->setBalance($response->balance);
     }
 }
